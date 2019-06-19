1d6f6664fa539957292373e0189de694413458be
Srinivas <Srinivas@Home-PC>
2019-06-19 16:21:28 +0530

    Second commit message

---
+++ TestProject/src/packageone/FirstProg.java
@@ -0,0 +1,13 @@
+package packageone;
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is the first program");
+		System.out.println("Sucessfully pushed ");
+
+	}
+
+}
